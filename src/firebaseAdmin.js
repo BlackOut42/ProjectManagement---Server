e7ab4e1b26bf849c20c7457d44ab367bfaf62ab2
BlackOut42 <thegal9989@gmail.com>
2024-07-26 17:07:44 +0300

    little change

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./config/firebaseAdminSDK.json");
+const path = require("path");
 
+// Path to the Firebase Admin SDK JSON file
+const serviceAccountPath = path.join(__dirname, "firebaseAdminSDK.json");
+
+// Initialize Firebase Admin with service account credentials
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(require(serviceAccountPath)),
 });
 
 module.exports = admin;
